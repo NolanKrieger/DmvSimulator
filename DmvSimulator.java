cf9a54f34cd1cce6ecb3ab9dd12623d2831f7dd7
Nolan Krieger <nolankrieger9@gmail.com>
2025-03-03 22:09:43 -0500

    changed wait to 200

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -3,7 +3,7 @@ public class DmvSimulator{
     public static void welcome(){
         System.out.println("Welcome to the DMV!");
 
-        int ticketNumber = (int)Math.floor(Math.random() * 100 + 1);
+        int ticketNumber = (int)Math.floor(Math.random() * 200 + 1);
 
         System.out.println("Your ticket number is " + ticketNumber + ", please wait until your number is called. Press enter to contine.");
 
@@ -11,7 +11,7 @@ public class DmvSimulator{
         scan.nextLine();
 
         int baseTicket;
-        if(ticketNumber == 100){
+        if(ticketNumber == 200){
             baseTicket = 1;
         }else{
             baseTicket = ticketNumber + 1;
@@ -20,7 +20,7 @@ public class DmvSimulator{
             System.out.println("Ticket number " + baseTicket + ", please come to the front desk.");
             if(baseTicket == ticketNumber){
                 break;
-            }else if(baseTicket == 100){
+            }else if(baseTicket == 200){
                 baseTicket = 1;
             }else{
                 baseTicket++;
